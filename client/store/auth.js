dc78ce1d82553d67b7d03a4df974a061cd0cfaee
Gary White <glw494@gmail.com>
2023-02-22 15:52:32 -0500

    Refactored auth thunks and reducer

--- client/store/auth.js
+++ client/store/auth.js
@@ -1,60 +1,44 @@
+/* eslint-disable consistent-return */
 import axios from 'axios';
 import history from '../history';
 
-const TOKEN = 'token';
+// eslint-disable-next-line default-param-last
+const auth = (state = {}, action) => {
+  if (action.type === 'SET_AUTH') {
+    return action.auth;
+  }
+  return state;
+};
 
-/**
- * ACTION TYPES
- */
-const SET_AUTH = 'SET_AUTH';
-
-/**
- * ACTION CREATORS
- */
-const setAuth = (auth) => ({ type: SET_AUTH, auth });
-
-/**
- * THUNK CREATORS
- */
 export const me = () => async (dispatch) => {
-  const token = window.localStorage.getItem(TOKEN);
+  const token = window.localStorage.getItem('token');
   if (token) {
     const res = await axios.get('/auth/me', {
       headers: {
         authorization: token,
       },
     });
-    return dispatch(setAuth(res.data));
+    return dispatch({ type: 'SET_AUTH', auth: res.data });
   }
 };
 
 export const authenticate = (username, password, method) => async (dispatch) => {
   try {
     const res = await axios.post(`/auth/${method}`, { username, password });
-    window.localStorage.setItem(TOKEN, res.data.token);
+    window.localStorage.setItem('token', res.data.token);
     dispatch(me());
   } catch (authError) {
-    return dispatch(setAuth({ error: authError }));
+    return dispatch({ type: 'SET_AUTH', auth: { error: authError } });
   }
 };
 
 export const logout = () => {
-  window.localStorage.removeItem(TOKEN);
+  window.localStorage.removeItem('token');
   history.push('/login');
   return {
-    type: SET_AUTH,
+    type: 'SET_AUTH',
     auth: {},
   };
 };
 
-/**
- * REDUCER
- */
-export default function (state = {}, action) {
-  switch (action.type) {
-    case SET_AUTH:
-      return action.auth;
-    default:
-      return state;
-  }
-}
+export default auth;
