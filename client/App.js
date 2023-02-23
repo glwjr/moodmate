bc2dc3692425c3dea3f1c229b362160cc72e5447
Gary White <glw494@gmail.com>
2023-02-22 22:49:46 -0500

    MUI config

--- client/App.js
+++ client/App.js
@@ -1,14 +1,27 @@
 /* eslint-disable react/jsx-filename-extension */
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
 import Navbar from './components/Navbar';
 import Routes from './Routes';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <Navbar />
-      <Routes />
-    </div>
+      <Container maxWidth="md">
+        <Routes />
+      </Container>
+    </ThemeProvider>
   );
 }
 
